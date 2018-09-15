8ad52cbc4791ddb3a822fb8455f420480164ced7
dmitrykholodok <dm.kholodok@gmail.com>
2018-09-15 11:50:21 +0300

    Updated db script

--- PIRIS/demo/db_script.sql
+++ PIRIS/demo/db_script.sql
@@ -1,98 +1,142 @@
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `piris`.`Client`
-DROP FOREIGN KEY `fk_Client_CitizenShip1`,
-DROP FOREIGN KEY `fk_Client_MaritalStatus1`,
-DROP FOREIGN KEY `fk_Client_Passport1`;
+-- -----------------------------------------------------
+-- Schema piris
+-- -----------------------------------------------------
 
-ALTER TABLE `piris`.`Address`
-DROP FOREIGN KEY `fk_Address_City1`;
+-- -----------------------------------------------------
+-- Schema piris
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `piris` DEFAULT CHARACTER SET utf8 ;
+USE `piris` ;
 
-ALTER TABLE `piris`.`Client`
-DROP COLUMN `disability_id`,
-DROP COLUMN `passport_id`,
-DROP COLUMN `address_id`,
-DROP COLUMN `ms_id`,
-DROP COLUMN `cs_id`,
-ADD COLUMN `cs_id` INT(10) UNSIGNED NOT NULL AFTER `email`,
-ADD COLUMN `ms_id` INT(10) UNSIGNED NOT NULL AFTER `cs_id`,
-ADD COLUMN `address_id` INT(10) UNSIGNED NOT NULL AFTER `ms_id`,
-ADD COLUMN `passport_id` INT(10) UNSIGNED NOT NULL AFTER `address_id`,
-ADD COLUMN `disability_id` INT(10) UNSIGNED NOT NULL AFTER `passport_id`,
-ADD INDEX `fk_Client_CitizenShip1_idx` (`cs_id` ASC),
-ADD INDEX `fk_Client_MaritalStatus1_idx` (`ms_id` ASC),
-ADD INDEX `fk_Client_Address1_idx` (`address_id` ASC),
-ADD INDEX `fk_Client_Passport1_idx` (`passport_id` ASC),
-ADD INDEX `fk_Client_Disability1_idx` (`disability_id` ASC),
-DROP INDEX `fk_Client_Disability1_idx` ,
-DROP INDEX `fk_Client_Passport1_idx` ,
-DROP INDEX `fk_Client_Address1_idx` ,
-DROP INDEX `fk_Client_MaritalStatus1_idx` ,
-DROP INDEX `fk_Client_CitizenShip1_idx` ;
-
+-- -----------------------------------------------------
+-- Table `piris`.`citizen_ship`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `piris`.`citizen_ship` (
-  `cs_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cs_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`cs_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `piris`.`marital_status`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `piris`.`marital_status` (
-  `ms_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ms_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`ms_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB;
 
-ALTER TABLE `piris`.`Address`
-DROP COLUMN `city_id`,
-ADD COLUMN `city_id` INT(10) UNSIGNED NOT NULL AFTER `tel`,
-ADD INDEX `fk_Address_City1_idx` (`city_id` ASC),
-DROP INDEX `fk_Address_City1_idx` ;
 
-DROP TABLE IF EXISTS `piris`.`marital_status` ;
+-- -----------------------------------------------------
+-- Table `piris`.`city`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piris`.`city` (
+  `city_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`city_id`))
+  ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `piris`.`citizen_ship` ;
 
-ALTER TABLE `piris`.`Client`
-DROP FOREIGN KEY `fk_Client_Address1`,
-DROP FOREIGN KEY `fk_Client_Disability1`;
-
-ALTER TABLE `piris`.`Client` ADD CONSTRAINT `fk_Client_CitizenShip1`
-  FOREIGN KEY (`cs_id`)
-  REFERENCES `piris`.`citizen_ship` (`cs_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Client_MaritalStatus1`
-  FOREIGN KEY (`ms_id`)
-  REFERENCES `piris`.`marital_status` (`ms_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Client_Address1`
-  FOREIGN KEY (`address_id`)
-  REFERENCES `piris`.`address` (`address_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Client_Passport1`
-  FOREIGN KEY (`passport_id`)
-  REFERENCES `piris`.`passport` (`passport_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_Client_Disability1`
-  FOREIGN KEY (`disability_id`)
-  REFERENCES `piris`.`disability` (`disability_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
-ALTER TABLE `piris`.`Address`
-ADD CONSTRAINT `fk_Address_City1`
+-- -----------------------------------------------------
+-- Table `piris`.`address`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piris`.`address` (
+  `address_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `address` VARCHAR(45) NOT NULL,
+  `tel` VARCHAR(45) NULL,
+  `city_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`address_id`),
+  INDEX `fk_Address_City1_idx` (`city_id` ASC),
+  CONSTRAINT `fk_Address_City1`
   FOREIGN KEY (`city_id`)
   REFERENCES `piris`.`city` (`city_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `piris`.`passport`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piris`.`passport` (
+  `passport_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `series` VARCHAR(45) NOT NULL,
+  `number` CHAR(2) NOT NULL,
+  `number_id` VARCHAR(45) NOT NULL,
+  `issued_by` VARCHAR(45) NOT NULL,
+  `issue_date` DATE NOT NULL,
+  `reg_addr` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`passport_id`))
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `piris`.`disability`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piris`.`disability` (
+  `disability_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `value` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`disability_id`))
+  ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `piris`.`client`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `piris`.`client` (
+  `client_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `surname` VARCHAR(45) NOT NULL,
+  `patronymic` VARCHAR(45) NOT NULL,
+  `sex` TINYINT NOT NULL,
+  `date_of_birth` DATE NOT NULL,
+  `is_pensioner` TINYINT NOT NULL,
+  `monthly_income` INT UNSIGNED NULL,
+  `place_of_birth` VARCHAR(45) NOT NULL,
+  `mob_tel` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `cs_id` INT UNSIGNED NOT NULL,
+  `ms_id` INT UNSIGNED NOT NULL,
+  `address_id` INT UNSIGNED NOT NULL,
+  `passport_id` INT UNSIGNED NOT NULL,
+  `disability_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`client_id`),
+  INDEX `fk_Client_CitizenShip1_idx` (`cs_id` ASC),
+  INDEX `fk_Client_MaritalStatus1_idx` (`ms_id` ASC),
+  INDEX `fk_Client_Address1_idx` (`address_id` ASC),
+  INDEX `fk_Client_Passport1_idx` (`passport_id` ASC),
+  INDEX `fk_Client_Disability1_idx` (`disability_id` ASC),
+  CONSTRAINT `fk_Client_CitizenShip1`
+  FOREIGN KEY (`cs_id`)
+  REFERENCES `piris`.`citizen_ship` (`cs_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Client_MaritalStatus1`
+  FOREIGN KEY (`ms_id`)
+  REFERENCES `piris`.`marital_status` (`ms_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Client_Address1`
+  FOREIGN KEY (`address_id`)
+  REFERENCES `piris`.`address` (`address_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Client_Passport1`
+  FOREIGN KEY (`passport_id`)
+  REFERENCES `piris`.`passport` (`passport_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Client_Disability1`
+  FOREIGN KEY (`disability_id`)
+  REFERENCES `piris`.`disability` (`disability_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
